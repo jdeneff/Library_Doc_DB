3b8dc8a1e252f6b3d81e4fda9756c5f381ebe735
jdeneff <jacob.deneff@gmail.com>
2023-08-13 21:39:19 -0600

    Finished CRUD Deleted older code and test JSON files

--- db_code/db.py
+++ db_code/db.py
@@ -6,25 +6,57 @@ class DocDB:
         self._next_id = None
     
     def insert(self, data:dict):
+        """
+        Get a new document id then call the updater to add the new document in.
+        Should I check for uniqueness of id or document?
+        """
         doc_id = self._get_next_id()
         def updater(db:dict):
             db[doc_id] = data
+        self._update_db(updater)
+
+    def get(self, doc_id:int):
+        db:dict = self._read_db()
+        if str(doc_id) in db.keys():
+            return db[str(doc_id)]
+        else:
+            print("No such document")
+    
+    def get_all(self):
+        db:dict = self._read_db()
+        docs = []
+        for key in db.keys(): docs.append(key)
+        return docs
+    
+    def update(self, doc_id:int, new_data:dict):
+        def updater(db:dict):
+            doc:dict = db[doc_id]
+            for key, item in new_data.items(): doc[key] = item
+            db[doc_id] = doc
+        self._update_db(updater)
+
+    def delete(self, doc_id):
+        def updater(db:dict):
+            db.pop(doc_id)
+        self._update_db(updater)
 
     def _get_next_id(self):
         db:dict = self._read_db()
 
         if self._next_id is not None:
             next_id = self._next_id
-        elif db == {}:
+            self._next_id = next_id +1
+            return next_id
+        
+        if db == {}:
             next_id = 1
         else:
             max_id = max(int(i) for i in db.keys())
             next_id = max_id + 1
-
         self._next_id = next_id +1
         return next_id
     
-    def _update_shelf(self, updater):
+    def _update_db(self, updater):
         # Read in from JSON
         raw_db:dict = self._read_db()
         # Convert to form expected by db
@@ -33,7 +65,7 @@ class DocDB:
         updater(db)
         new_db = {str(doc_id):doc for doc_id, doc in db.items()}
         # Write to storage file
-        self._storage.write(db)
+        self._storage.write(new_db)
 
     def _read_db(self):
         return self._storage.read()
