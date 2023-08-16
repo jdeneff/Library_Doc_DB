acfb1c6b6c4bd794e9418cce57f9229d94d43d7b
jdeneff <jacob.deneff@gmail.com>
2023-08-15 20:23:05 -0600

    Rewrote entirely, need to add collection support

--- db_code/db.py
+++ db_code/db.py
@@ -1,71 +1,32 @@
 from storage import JSONStorage
 
 class DocDB:
-    def __init__(self, path:str) -> None:
-        self._storage = JSONStorage(path)
+    def __init__(self) -> None:
+        self._storage = JSONStorage()
+        self._name = None
+        self._dir_path = None
         self._next_id = None
+        self._index = {}
     
-    def insert(self, data:dict):
-        """
-        Get a new document id then call the updater to add the new document in.
-        Should I check for uniqueness of id or document?
-        """
-        doc_id = self._get_next_id()
-        def updater(db:dict):
-            db[doc_id] = data
-        self._update_db(updater)
-
-    def get(self, doc_id:int):
-        db:dict = self._read_db()
-        if str(doc_id) in db.keys():
-            return db[str(doc_id)]
-        else:
-            print("No such document")
+    def create(self, name:str, dir_path:str):
+        # Need to make it error when the file already exists - use return from storage
+        self._name = name
+        self._dir_path = dir_path
+        file_path = self._dir_path + "/" + self._name + "_index.json"
+        self._storage.db_create(self._name, file_path)
     
-    def get_all(self):
-        db:dict = self._read_db()
-        docs = []
-        for key in db.keys(): docs.append(key)
-        return docs
+    def open(self, path:str):
+        # Need to make it error when the file is not found - use return from storage
+        data = self._storage.read(path)
+        self._name = data["name"]
+        self._index = data["index"]
+        self._dir_path = path[0:path.rfind("/")+1]
     
-    def update(self, doc_id:int, new_data:dict):
-        def updater(db:dict):
-            doc:dict = db[doc_id]
-            for key, item in new_data.items(): doc[key] = item
-            db[doc_id] = doc
-        self._update_db(updater)
-
-    def delete(self, doc_id):
-        def updater(db:dict):
-            db.pop(doc_id)
-        self._update_db(updater)
-
-    def _get_next_id(self):
-        db:dict = self._read_db()
-
-        if self._next_id is not None:
-            next_id = self._next_id
-            self._next_id = next_id +1
-            return next_id
-        
-        if db == {}:
-            next_id = 1
-        else:
-            max_id = max(int(i) for i in db.keys())
-            next_id = max_id + 1
-        self._next_id = next_id +1
-        return next_id
-    
-    def _update_db(self, updater):
-        # Read in from JSON
-        raw_db:dict = self._read_db()
-        # Convert to form expected by db
-        db = {int(doc_id):doc for doc_id, doc in raw_db.items()}
-        # Update then convert back to form for JSON
-        updater(db)
-        new_db = {str(doc_id):doc for doc_id, doc in db.items()}
-        # Write to storage file
-        self._storage.write(new_db)
-
-    def _read_db(self):
-        return self._storage.read()
+if __name__ == "__main__":
+    db = DocDB()
+    db.create("test", "C:/Users/Admin/Jacob_Docs")
+    db_two = DocDB()
+    db_two.open("C:/Users/Admin/Jacob_Docs/test_index.json")
+    print(db_two._name)
+    print(db_two._index)
+    print(db_two._dir_path)
