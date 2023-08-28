40e5393765641a26415326e46053c632620edce2
jdeneff <jacob.deneff@gmail.com>
2023-08-28 13:04:42 -0600

    Changed to use file path

--- db_code/storage.py
+++ db_code/storage.py
@@ -5,18 +5,16 @@ class JSONStorage:
     def __init__(self):
         pass
 
-    def exists(self, dir_name:str, file_name:str):
+    def exists(self, file_path:str):
         """Check if a file exists based on directory and file name"""
-        file_path = os.path.join(dir_name, file_name)
         if os.path.exists(file_path):
             return True
         else:
             return False
 
-    def jsonfile_create(self, data:dict, dir_name:str, file_name:str):
+    def jsonfile_create(self, data:dict, file_path):
         """Create a json file with some data if it does not exist already"""
         ser_data = json.dumps(data)
-        file_path = os.path.join(dir_name, file_name)
         if not os.path.exists(file_path):
             with open(file_path, "a") as f:
                 f.write(ser_data)
@@ -25,9 +23,8 @@ class JSONStorage:
             print(f"Error writing file to {file_path}")
             return False
 
-    def read(self, dir_name:str, file_name:str):
+    def read(self, file_path:str):
         """Read from a json file and return an error if it does not exist or isn't json formatted"""
-        file_path = os.path.join(dir_name, file_name)
         if os.path.exists(file_path):
             with open(file_path, "r+") as f:
                 try:
@@ -39,10 +36,9 @@ class JSONStorage:
         else:
             print("File not found")
 
-    def write(self, data:dict, dir_name:str, file_name:str):
+    def write(self, data:dict, file_path):
         """Overwrite an existing json file then truncate to new length, if shorter than before"""
         serialized = json.dumps(data, indent=4)
-        file_path = os.path.join(dir_name, file_name)
         if os.path.exists(file_path):
             with open(file_path, "r+") as f:
                 f.seek(0)
