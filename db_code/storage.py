2eb5c36a7e4c35f31b371030c41a1ff29f29edf2
jdeneff <jacob.deneff@gmail.com>
2023-08-31 16:14:22 -0600

    Added type annotations to functions

--- db_code/storage.py
+++ db_code/storage.py
@@ -5,14 +5,14 @@ class JSONStorage:
     def __init__(self):
         pass
 
-    def exists(self, file_path:str):
+    def exists(self, file_path:str) -> bool:
         """Check if a file exists based on directory and file name"""
         if os.path.exists(file_path):
             return True
         else:
             return False
 
-    def jsonfile_create(self, data:dict, file_path):
+    def jsonfile_create(self, data:dict, file_path) -> bool:
         """Create a json file with some data if it does not exist already"""
         ser_data = json.dumps(data)
         if not os.path.exists(file_path):
@@ -23,7 +23,7 @@ class JSONStorage:
             print(f"Error writing file to {file_path}")
             return False
 
-    def read(self, file_path:str):
+    def read(self, file_path:str) -> dict:
         """Read from a json file and return an error if it does not exist or isn't json formatted"""
         if os.path.exists(file_path):
             with open(file_path, "r+") as f:
@@ -31,12 +31,12 @@ class JSONStorage:
                     data = json.load(f)
                 except:
                     print("Not json formatted")
-                    return
+                    return {}
             return data
         else:
             print("File not found")
 
-    def write(self, data:dict, file_path):
+    def write(self, data:dict, file_path) -> None:
         """Overwrite an existing json file then truncate to new length, if shorter than before"""
         serialized = json.dumps(data, indent=4)
         if os.path.exists(file_path):
